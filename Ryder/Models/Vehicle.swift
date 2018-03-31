510b46a1147ec20cca9c576791c36ead1271e922
ktan17 <Kevin.Tan@gpiglobal.com>
2018-03-31 15:31:37 -0700

    Implemented TicketViewCell

---
+++ Ryder/Models/Vehicle.swift
@@ -0,0 +1,16 @@
+//
+//  Vehicle.swift
+//  Ryder
+//
+//  Created by Kevin Tan on 3/31/18.
+//  Copyright © 2018 Kevin Tan. All rights reserved.
+//
+
+import Foundation
+
+class Vehicle: NSObject {
+    
+    var distance: Double!
+    var url: String!
+    
+}
--- Ryder/View Controllers/MainTableViewController.swift
+++ Ryder/View Controllers/MainTableViewController.swift
@@ -12,12 +12,18 @@ import Gimbal
 class MainTableViewController: UITableViewController, GMBLBeaconManagerDelegate {
 
     lazy var beaconManager = GMBLBeaconManager()
+    var vehiclesInRange = [Vehicle]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         self.beaconManager.delegate = self
+        vehiclesInRange.append(Vehicle())
+        vehiclesInRange.append(Vehicle())
+        vehiclesInRange.append(Vehicle())
+        
+        tableView.separatorStyle = .none
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -29,6 +35,42 @@ class MainTableViewController: UITableViewController, GMBLBeaconManagerDelegate 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: UITableViewDataSource/UITableViewDelegate methods
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return vehiclesInRange.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "TicketViewCell") {
+            return cell
+        }
+        
+        let objects = Bundle.main.loadNibNamed("TicketViewCell", owner: self, options: nil)
+        for object in objects ?? [] {
+            if let cell = object as? TicketViewCell {
+                cell.selectionStyle = .none
+                return cell
+            }
+        }
+        
+        return UITableViewCell(style: .default, reuseIdentifier: "boob")
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 212
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("im gay")
     }
     
     // MARK: GMBLBeaconManagerDelegate methods
