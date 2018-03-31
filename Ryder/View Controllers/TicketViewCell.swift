0ca7be0e1b1edab789476e88d97f5b7c66a415eb
dustinnewman98 <dustinnewman98@gmail.com>
2018-03-31 14:47:19 -0700

    TicketViewCell added

---
+++ Ryder/View Controllers/TicketViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TicketViewCell.swift
+//  Ryder
+//
+//  Created by Dustin Newman on 3/31/18.
+//  Copyright © 2018 Kevin Tan. All rights reserved.
+//
+
+import UIKit
+
+class TicketViewCell: UITableViewCell {
+    
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var logoImageView: UIImageView!
+    @IBOutlet var transitTypeLabel: UILabel!
+    @IBOutlet var transitNumberLabel: UILabel!
+    @IBOutlet var starImageView: UIImageView!
+    @IBOutlet var nextLabel: UILabel!
+    @IBOutlet var nextLocationLabel: UILabel!
+    
+    
+    
+}
