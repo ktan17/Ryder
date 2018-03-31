9f18a73462ee0f9ed3bd333f2e7a8f0f9f34ce52
ktan17 <Kevin.Tan@gpiglobal.com>
2018-03-30 23:12:38 -0700

    Initial Commit

---
+++ RyderTests/RyderTests.swift
@@ -0,0 +1,36 @@
+//
+//  RyderTests.swift
+//  RyderTests
+//
+//  Created by Kevin Tan on 3/30/18.
+//  Copyright © 2018 Kevin Tan. All rights reserved.
+//
+
+import XCTest
+@testable import Ryder
+
+class RyderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
