7a0ef86759b70c71e012aac52dd25f8c1f3c24d4
ktan17 <Kevin.Tan@gpiglobal.com>
2018-04-01 10:10:35 -0700

    bitch wanna party with cards

--- Ryder/AppDelegate.swift
+++ Ryder/AppDelegate.swift
@@ -51,6 +51,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        application.applicationIconBadgeNumber = 0 // For Clear Badge Counts
+        let center = UNUserNotificationCenter.current()
+        center.removeAllDeliveredNotifications() // To remove all delivered notifications
+        center.removeAllPendingNotificationRequests()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
