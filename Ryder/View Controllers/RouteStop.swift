c264e7ca87b77faa0e225c131a23443a733baee8
Hannah Elarabawy <helarabawy@ucla.edu>
2018-03-31 19:08:09 -0700

    details page

---
+++ Ryder/View Controllers/RouteStop.swift
@@ -0,0 +1,21 @@
+//
+//  RouteStop.swift
+//  Ryder
+//
+//  Created by Hannah Elarabawy on 3/31/18.
+//  Copyright © 2018 Kevin Tan. All rights reserved.
+//
+
+import UIKit
+
+class RouteStop: UIView {
+    
+    var stopLabel: UILabel!
+    var timeLabel: UILabel!
+    var dotView: UIView!
+    var lineView: UIView!
+    
+    init(stopLabel: String, timeLabel: String) {
+        stopLabel.text = 
+    }
+}
